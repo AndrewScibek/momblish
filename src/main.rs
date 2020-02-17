7faf6b37f658b3c9cdc98270f75da960bf2e77bc
Andrew Scibek <andrew.scibek@gmail.com>
2020-02-16 19:54:29 -0800

    Add readme

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ mod momblish;
 fn main() {
     
     let momblish = momblish::Momblish::init("./corpus/corpus.json");
-    for _ in 0..100000 {
-        println!("{}",momblish.word(10));
+    for _ in 0..10000000 {
+        momblish.word(10);
     }
-    
 }
